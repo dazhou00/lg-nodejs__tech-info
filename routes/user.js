21b11859c9f6c0376fefb3acf1535d9763953dd0
周健 <215320172@qq.com>
2021-10-10 19:33:21 +0800

    用户模块

--- routes/user.js
+++ routes/user.js
@@ -8,4 +8,19 @@ router.post('/', (req, res, next) => {
   res.send('用户注册')
 })
 
+// 获取用户
+router.get('/', (req, res, next) => {
+  res.send('获取用户')
+})
+
+// 编辑用户
+router.put('/', (req, res, next) => {
+  res.send('编辑用户')
+})
+
+// 删除用户
+router.delete('/', (req, res, next) => {
+  res.send('删除用户')
+})
+
 module.exports = router
