7ab7c342fdb9ecdcb16c70b311670c7e705b57cd
周健 <215320172@qq.com>
2021-10-10 18:58:07 +0800

    基础中间件

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const config = require('./config')
 
 const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
 
 const app = express()
 
-app.get('/', (req, res) => {
+// 引入中间件
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+
+app.post('/', (req, res) => {
+  console.log(req.body);
   res.end('1234')
 })
 
