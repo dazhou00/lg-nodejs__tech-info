1633bc96a0ef1375a77926ba341cbdaba474b0a3
周健 <215320172@qq.com>
2021-10-10 19:53:29 +0800

    分类模块

--- routes/categories.js
+++ routes/categories.js
@@ -4,5 +4,29 @@
 
 const router = require('express').Router()
 
+// 获取全部
+router.get('/', (req, res, next) => {
+  res.send('获取全部')
+})
+
+// 获取一个
+router.get('/:cid', (req, res, next) => {
+  res.send(`获取${req.params.cid}`)
+})
+
+// 添加新的
+router.post('/', (req, res, next) => {
+  res.send('添加')
+})
+
+// 编辑
+router.put('/:cid', (req, res, next) => {
+  res.send(`编辑${req.params.cid}`)
+})
+
+// 删除
+router.delete('/:cid', (req, res, next) => {
+  res.send(`删除${req.params.cid}`)
+})
 
 module.exports = router
