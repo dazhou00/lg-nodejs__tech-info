7b3e5d4989b2dd501a680f4f08a0f2aa0f89f33e
zhou jian <215320172@qq.com>
2021-12-03 23:18:23 +0800

    使用tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: {
+    enabled:true,
+    content:[
+      './views/**/*.html',
+      './views/*.html'
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
